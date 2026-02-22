698fedf3e694f8f4388237be1b1a27b290fc9958
João <joaolucassilvasimoes123@gmail.com>
2026-02-22 00:29:18 -0300

    Criando uma function que valida Cpf's

--- src/util/Util.java
+++ src/util/Util.java
@@ -95,4 +95,38 @@ public class Util {
             }
         }
     }
+
+
+    public static boolean validarCPF(String cpf) {
+        cpf = cpf.replaceAll("\\D", "");
+
+        if (cpf.length() != 11) return false;
+        if (cpf.matches("(\\d)\\1{10}")) return false;
+
+        int soma = 0;
+        int peso = 10;
+
+        for (int i = 0; i < 9; i++) {
+            soma += (cpf.charAt(i) - '0') * peso--;
+        }
+
+        int digito1 = 11 - (soma % 11);
+        if (digito1 >= 10) digito1 = 0;
+        if (digito1 != (cpf.charAt(9) - '0')) return false;
+
+        soma = 0;
+        peso = 11;
+
+        for (int i = 0; i < 10; i++) {
+            soma += (cpf.charAt(i) - '0') * peso--;
+        }
+
+        int digito2 = 11 - (soma % 11);
+        if (digito2 >= 10) digito2 = 0;
+
+        return digito2 == (cpf.charAt(10) - '0');
+    }
+
+
+
 }
