9c8a166b85629e0bc0599b7cb3a0d30dd8c25285
João <joaolucassilvasimoes123@gmail.com>
2026-02-22 00:19:51 -0300

    Implementando a classe RanqueDAO

--- src/dao/RanqueDAO.java
+++ src/dao/RanqueDAO.java
@@ -1,4 +1,32 @@
 package dao;
 
+import db.DataBase;
+import model.Ranque;
+
+import java.util.ArrayList;
+
 public class RanqueDAO {
+
+    DataBase dataBase = DataBase.getInstance();
+
+    public RanqueDAO () {}
+
+
+    public ArrayList<Ranque> getAll() {
+        ArrayList<Ranque> arrayReturn = dataBase.getRanque();
+        return arrayReturn;
+    }
+
+    public void limpaDados() {
+        ArrayList<Ranque> ranque = dataBase.getRanque();
+
+        for(int i = 0;i < ranque.size();i++) {
+           ranque.remove(i);
+        }
+    }
+
+    public boolean inserirRanque(Ranque ranqueInput) {
+        dataBase.add(ranqueInput);
+        return true;
+    }
 }
